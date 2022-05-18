cccabdc351e07215bcbbfcfc68d9eed609de50ad
Freilichtbuehne <heinzmann.samuel@web.de>
2022-05-18 15:33:01 +0200

    .

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -1,19 +1,18 @@
 <?php
-// src/Controller/number.php
+// src/Controller/LuckyController.php
 namespace App\Controller;
-
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LuckyController extends AbstractController
 {
+    /**
+    * @Route("/number")
+    */
     public function number(): Response
     {
         $number = random_int(0, 100);
-
-        /*return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );*/
         return $this->render('notes/notes.html.twig', [
             'number' => $number,
         ]);
