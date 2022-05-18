408e5b2778178a74d99f2029aa2a4189715f7da8
Freilichtbuehne <heinzmann.samuel@web.de>
2022-05-18 15:53:57 +0200

    .

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -1,20 +1,20 @@
 <?php
-// src/Controller/LuckyController.php
+// src/Controller/LoginController.php
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class LuckyController extends AbstractController
+class LoginController extends AbstractController
 {
     /**
-    * @Route("/number")
+    * @Route("/login")
     */
-    public function number(): Response
+    public function index(): Response
     {
         $number = random_int(0, 100);
-        return $this->render('notes/notes.html.twig', [
-            'number' => $number,
+        return $this->render('login.html.twig', [
+            //'number' => $number,
         ]);
     }
 }
