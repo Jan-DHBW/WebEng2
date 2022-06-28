d42f58b4db6643f5c5e2763b08ca6150a1cd6989
RaizorHD <s211289@student.dhbw-mannheim.de>
2022-06-28 08:29:57 +0200

    Test

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -26,8 +26,8 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('surname')
-            ->add('lastname')
+            ->add('surname', null, array('label' => false))
+            ->add('lastname', null, array('label' => false))
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -44,7 +44,7 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
-            ])
+            ], null, array('label' => false))
         ;
     }
 
