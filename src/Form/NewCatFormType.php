8f486aebdc424ecbae0190e00b05925b1f46086e
Jan-DHBW <ackermannjan06@gmail.com>
2022-06-29 15:52:04 +0200

    asd

--- src/Form/NewCatFormType.php
+++ src/Form/NewCatFormType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class NewCatFormType extends AbstractType
 {
