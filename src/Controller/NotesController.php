e0de7622a542c7208939f33b56bf44da03c8b091
Freilichtbuehne <heinzmann.samuel@web.de>
2022-07-01 13:22:35 +0200

    .

--- src/Controller/NotesController.php
+++ src/Controller/NotesController.php
@@ -72,6 +72,7 @@ class NotesController extends AbstractController
             // do anything else you need here, like send an email
             return $this->redirectToRoute('notes');
         }
+        print_r($notes);
 
         return $this->render('notes.html.twig', [
             'notes' => $notes,
