22f79b836f7537ebf7e1e4a06b2884ed8c75bed8
RaizorHD <91869631+RaizorHD@users.noreply.github.com>
2022-07-02 11:18:43 +0200

    Zurück

--- src/Form/MoveNoteFormType.php
+++ src/Form/MoveNoteFormType.php
@@ -34,6 +34,7 @@ public function buildForm(FormBuilderInterface $builder, array $options){
     $builder
         ->add('category', ChoiceType::class, [
             'label' => false,
+            'attr' => array('class' => 'form-select'),
             'choices' => $categories,
             'choice_label' => 'name',
             'choice_value' => 'id',
