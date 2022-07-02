7b6e7872abad5264a7b728f87943f48d06391702
Jan-DHBW <ackermannjan06@gmail.com>
2022-07-02 10:57:48 +0200

    asd

--- src/Form/MoveNoteFormType.php
+++ src/Form/MoveNoteFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use App\Entity\Note;
+use App\Entity\moveTask;
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
